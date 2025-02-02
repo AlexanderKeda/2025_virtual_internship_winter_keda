565ecc9b95a35f9105cf12de15bd62a600c4cd73
Alexander Keda <alex.keda94@yandex.ru>
2025-02-02 17:52:52 +0300

    STEP 019 - FIX

--- student_alexander_keda/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
+++ student_alexander_keda/src/main/java/org/javaguru/travel/insurance/core/DateTimeService.java
@@ -3,7 +3,7 @@ package org.javaguru.travel.insurance.core;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
-public class DateTimeService {
+class DateTimeService {
     public static long calculateDaysBetween(LocalDate dateFrom, LocalDate dateTo) {
         return ChronoUnit.DAYS.between(dateFrom, dateTo);
     }
