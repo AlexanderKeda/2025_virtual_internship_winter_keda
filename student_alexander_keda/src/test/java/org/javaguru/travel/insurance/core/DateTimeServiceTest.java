03bedd9fcc2f71c479a104885f2302e5b907b328
Alexander Keda <alex.keda94@yandex.ru>
2025-02-02 20:17:08 +0300

    STEP 019 - FIX

--- student_alexander_keda/src/test/java/org/javaguru/travel/insurance/core/DateTimeServiceTest.java
+++ student_alexander_keda/src/test/java/org/javaguru/travel/insurance/core/DateTimeServiceTest.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTimeServiceTest {
-    static DateTimeService dateTimeService;
 
     @Test
     void shouldDaysBetweenBePositive() {
