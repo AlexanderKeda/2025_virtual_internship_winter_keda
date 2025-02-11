b45a25d36cbaf4d0ca6c7c7722b0b2000de5e119
Alexander Keda <alex.keda94@yandex.ru>
2025-02-11 18:07:39 +0300

    STEP 048 - DONE

--- student_alexander_keda/src/test/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_alexander_keda/src/test/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -1,6 +1,5 @@
 package org.javaguru.travel.insurance.rest;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +11,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -25,23 +22,240 @@ class TravelCalculatePremiumControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void simpleRestControllerTest() throws Exception {
+    void shouldResponseCorrect() throws Exception {
+        String requestJson = """
+                {
+                "personFirstName" : "Ivan",
+                "personLastName" : "Ivanov",
+                "agreementDateFrom" : "2025-02-16",
+                "agreementDateTo" : "2025-02-25"
+                }
+                """;
+        String expectedResponseJson = """
+                 {
+                 "errors": null,
+                 "personFirstName": "Ivan",
+                 "personLastName": "Ivanov",
+                 "agreementDateFrom": "2025-02-16",
+                 "agreementDateTo": "2025-02-25",
+                 "agreementPrice": 9
+                 }
+                """;
+
         mockMvc.perform(MockMvcRequestBuilders.post("/insurance/travel/")
-                .content("""
-                        {
-                        "personFirstName" : "Ivan",
-                        "personLastName" : "Ivanov",
-                        "agreementDateFrom" : "2025-02-11",
-                        "agreementDateTo" : "2025-02-15"
-                        }
-                        """)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                        .content(requestJson)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("personFirstName", Matchers.is("Ivan")))
-                .andExpect(jsonPath("personLastName", Matchers.is("Ivanov")))
-                .andExpect(jsonPath("agreementDateFrom", Matchers.is("2025-02-11")))
-                .andExpect(jsonPath("agreementDateTo", Matchers.is("2025-02-15")))
-                .andExpect(jsonPath("agreementPrice", Matchers.is(4)));
+                .andExpect(content().json(expectedResponseJson));
     }
 
+    @Test
+    void shouldReturnErrorWhenFirstNameIsEmpty() throws Exception {
+        String requestJson = """
+                {
+                "personFirstName" : "",
+                "personLastName" : "Ivanov",
+                "agreementDateFrom" : "2025-02-16",
+                "agreementDateTo" : "2025-02-25"
+                }
+                """;
+        String expectedResponseJson = """
+                 {
+                                     "errors": [
+                                         {
+                                             "field": "personFirstName",
+                                             "message": "Must not be empty!"
+                                         }
+                                     ],
+                                     "personFirstName": null,
+                                     "personLastName": null,
+                                     "agreementDateFrom": null,
+                                     "agreementDateTo": null,
+                                     "agreementPrice": null
+                                 }
+                """;
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/insurance/travel/")
+                        .content(requestJson)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseJson));
+    }
+
+    @Test
+    void shouldReturnErrorWhenLastNameIsEmpty() throws Exception {
+        String requestJson = """
+                {
+                "personFirstName" : "Ivan",
+                "personLastName" : "",
+                "agreementDateFrom" : "2025-02-16",
+                "agreementDateTo" : "2025-02-25"
+                }
+                """;
+        String expectedResponseJson = """
+                 {
+                                    "errors": [
+                                        {
+                                            "field": "personLastName",
+                                            "message": "Must not be empty!"
+                                        }
+                                    ],
+                                    "personFirstName": null,
+                                    "personLastName": null,
+                                    "agreementDateFrom": null,
+                                    "agreementDateTo": null,
+                                    "agreementPrice": null
+                                }
+                """;
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/insurance/travel/")
+                        .content(requestJson)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseJson));
+    }
+
+    @Test
+    void shouldReturnErrorWhenDateFromIsEmpty() throws Exception {
+        String requestJson = """
+                {
+                "personFirstName" : "Ivan",
+                "personLastName" : "Ivanov",
+                "agreementDateFrom" : "",
+                "agreementDateTo" : "2025-02-25"
+                }
+                """;
+        String expectedResponseJson = """
+                 {
+                                    "errors": [
+                                        {
+                                            "field": "agreementDateFrom",
+                                            "message": "Must not be empty!"
+                                        }
+                                    ],
+                                    "personFirstName": null,
+                                    "personLastName": null,
+                                    "agreementDateFrom": null,
+                                    "agreementDateTo": null,
+                                    "agreementPrice": null
+                                }
+                """;
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/insurance/travel/")
+                        .content(requestJson)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseJson));
+    }
+
+    @Test
+    void shouldReturnErrorWhenDateToIsEmpty() throws Exception {
+        String requestJson = """
+                {
+                "personFirstName" : "Ivan",
+                "personLastName" : "Ivanov",
+                "agreementDateFrom" : "2025-02-16",
+                "agreementDateTo" : ""
+                }
+                """;
+        String expectedResponseJson = """
+                 {
+                                     "errors": [
+                                         {
+                                             "field": "agreementDateTo",
+                                             "message": "Must not be empty!"
+                                         }
+                                     ],
+                                     "personFirstName": null,
+                                     "personLastName": null,
+                                     "agreementDateFrom": null,
+                                     "agreementDateTo": null,
+                                     "agreementPrice": null
+                                 }
+                """;
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/insurance/travel/")
+                        .content(requestJson)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseJson));
+    }
+
+    @Test
+    void shouldReturnErrorsWhenAllFieldsAreEmpty() throws Exception {
+        String requestJson = """
+                {
+                "personFirstName" : "",
+                "personLastName" : "",
+                "agreementDateFrom" : "",
+                "agreementDateTo" : ""
+                }
+                """;
+        String expectedResponseJson = """
+                 {
+                                     "errors": [
+                                         {
+                                             "field": "personFirstName",
+                                             "message": "Must not be empty!"
+                                         },
+                                         {
+                                             "field": "personLastName",
+                                             "message": "Must not be empty!"
+                                         },
+                                         {
+                                             "field": "agreementDateFrom",
+                                             "message": "Must not be empty!"
+                                         },
+                                         {
+                                             "field": "agreementDateTo",
+                                             "message": "Must not be empty!"
+                                         }
+                                     ],
+                                     "personFirstName": null,
+                                     "personLastName": null,
+                                     "agreementDateFrom": null,
+                                     "agreementDateTo": null,
+                                     "agreementPrice": null
+                                 }
+                """;
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/insurance/travel/")
+                        .content(requestJson)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseJson));
+    }
+
+    @Test
+    void shouldReturnErrorWhenDateToIsBeforeDateFrom() throws Exception {
+        String requestJson = """
+                {
+                "personFirstName" : "Ivan",
+                "personLastName" : "Ivanov",
+                "agreementDateFrom" : "2025-02-25",
+                "agreementDateTo" : "2025-02-16"
+                }
+                """;
+        String expectedResponseJson = """
+                 {
+                                     "errors": [
+                                         {
+                                             "field": "agreementDateTo",
+                                             "message": "Must be after DataFrom!"
+                                         }
+                                     ],
+                                     "personFirstName": null,
+                                     "personLastName": null,
+                                     "agreementDateFrom": null,
+                                     "agreementDateTo": null,
+                                     "agreementPrice": null
+                                 }
+                """;
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/insurance/travel/")
+                        .content(requestJson)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseJson));
+    }
 }
