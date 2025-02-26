162046726b402c45b1ae42527f283611780a86ca
Alexander Keda <alex.keda94@yandex.ru>
2025-02-26 21:45:34 +0300

    STEP 074 - DONE

---
+++ student_alexander_keda/database/mySqlDataScript.sql
@@ -0,0 +1,14 @@
+INSERT INTO classifiers (title, description)
+VALUE ('RISK_TYPE', 'travel policy risk type classifier');
+
+SELECT id INTO @RISK_TYPE_ID
+FROM classifiers
+WHERE title = 'RISK_TYPE';
+
+INSERT INTO classifier_values (classifier_id, ic, description)
+VALUES (@RISK_TYPE_ID, 'TRAVEL_MEDICAL ', 'travel policy medical risk'),
+	(@RISK_TYPE_ID, 'TRAVEL_CANCELLATION ', 'travel policy trip cancellation risk'),
+	(@RISK_TYPE_ID, 'TRAVEL_LOSS_BAGGAGE ', 'travel policy baggage lose risk'),
+	(@RISK_TYPE_ID, 'TRAVEL_THIRD_PARTY_LIABILITY ', 'travel policy third party liability risk'),
+	(@RISK_TYPE_ID, 'TRAVEL_EVACUATION ', 'travel policy evacuation risk'),
+	(@RISK_TYPE_ID, 'TRAVEL_SPORT_ACTIVITIES ', 'travel policy sport activities risk');
