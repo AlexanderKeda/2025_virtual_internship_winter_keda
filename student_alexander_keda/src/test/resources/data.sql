65d26988d2ae70b03fe6383564fadf53ab4b7082
Alexander Keda <alex.keda94@yandex.ru>
2025-03-14 10:44:07 +0300

    STEP 105 - DONE

--- student_alexander_keda/src/test/resources/data.sql
+++ student_alexander_keda/src/test/resources/data.sql
@@ -10,3 +10,13 @@ VALUES (@RISK_TYPE_ID, 'TRAVEL_MEDICAL', 'travel policy medical risk'),
 	(@RISK_TYPE_ID, 'TRAVEL_THIRD_PARTY_LIABILITY', 'travel policy third party liability risk'),
 	(@RISK_TYPE_ID, 'TRAVEL_EVACUATION', 'travel policy evacuation risk'),
 	(@RISK_TYPE_ID, 'TRAVEL_SPORT_ACTIVITIES', 'travel policy sport activities risk');
+
+INSERT INTO classifiers (title, description)
+VALUES ('COUNTRY', 'Country classifier');
+
+SET @COUNTRY_TYPE_ID = (SELECT id FROM classifiers WHERE title = 'COUNTRY');
+
+INSERT INTO classifier_values (classifier_id, ic, description)
+VALUES (@COUNTRY_TYPE_ID, 'LATVIA', 'Country Latvia'),
+	(@COUNTRY_TYPE_ID, 'SPAIN', 'Country Spain'),
+	(@COUNTRY_TYPE_ID, 'JAPAN', 'Country Japan');
