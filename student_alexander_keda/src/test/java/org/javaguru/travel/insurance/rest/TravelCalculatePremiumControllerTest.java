36248be51f515b43c005081d4d520e8553195330
Alexander Keda <alex.keda94@yandex.ru>
2025-03-24 15:56:32 +0300

    STEP 129 - DONE

--- student_alexander_keda/src/test/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_alexander_keda/src/test/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -1,7 +1,8 @@
 package org.javaguru.travel.insurance.rest;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static uk.org.webcompere.modelassert.json.JsonAssertions.assertJson;
@@ -31,6 +35,25 @@ class TravelCalculatePremiumControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private static final int NUMBER_OF_TEST_CASES = 23;
+
+    static Stream<String> testCasesStream() {
+        return IntStream.rangeClosed(1, NUMBER_OF_TEST_CASES)
+                .mapToObj(i -> String.format("test_case_%02d", i));
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCasesStream")
+    void executeTestCases(String testCase) throws Exception {
+        runTestCase(testCase);
+    }
+
+    private void runTestCase(String testCase) throws Exception {
+        String requestPath = "/rest/" + testCase + "/request.json";
+        String expectedResponsePath = "/rest/" + testCase + "/response.json";
+        performAndCheck(requestPath, expectedResponsePath);
+    }
+
     private void performAndCheck(String requestPath, String expectedResponsePath) throws Exception {
         String requestJson = jsonFileReader.readJsonFromFile(requestPath);
 
@@ -47,144 +70,5 @@ class TravelCalculatePremiumControllerTest {
                 .keysInAnyOrder()
                 .arrayInAnyOrder()
                 .isEqualTo(expectedResponseJson);
-    }
-
-    @Test
-    void shouldReturnSuccessResponse() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_success.json",
-                "/rest/TravelCalculatePremiumResponse_success.json");
-    }
-
-
-    @Test
-    void shouldReturnErrorWhenFirstNameIsEmpty() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_firstName_not_provided.json",
-                "/rest/TravelCalculatePremiumResponse_firstName_not_provided.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenLastNameIsEmpty() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_lastName_not_provided.json",
-                "/rest/TravelCalculatePremiumResponse_lastName_not_provided.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenDateFromIsEmpty() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_dateFrom_not_provided.json",
-                "/rest/TravelCalculatePremiumResponse_dateFrom_not_provided.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenDateToIsEmpty() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_dateTo_not_provided.json",
-                "/rest/TravelCalculatePremiumResponse_dateTo_not_provided.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenSelectedRisksIsEmpty() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_selectedRisks_not_provided.json",
-                "/rest/TravelCalculatePremiumResponse_selectedRisks_not_provided.json");
-    }
-
-    @Test
-    void shouldReturnErrorsWhenAllFieldsAreEmpty() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_allFields_not_provided.json",
-                "/rest/TravelCalculatePremiumResponse_allFields_not_provided.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenDateToIsBeforeDateFrom() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_dateTo_lessThen_dateFrom.json",
-                "/rest/TravelCalculatePremiumResponse_dateTo_lessThen_dateFrom.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenDateFromIsInThePast() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_dateFrom_in_the_past.json",
-                "/rest/TravelCalculatePremiumResponse_dateFrom_in_the_past.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenDateToIsInThePast() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_dateTo_in_the_past.json",
-                "/rest/TravelCalculatePremiumResponse_dateTo_in_the_past.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenFirstNameIsNull() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_firstName_is_null.json",
-                "/rest/TravelCalculatePremiumResponse_firstName_is_null.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenLastNameIsNull() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_lastName_is_null.json",
-                "/rest/TravelCalculatePremiumResponse_lastName_is_null.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenRiskDoesNotExist() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_selectedRisks_does_not_exist.json",
-                "/rest/TravelCalculatePremiumResponse_selectedRisks_does_not_exist.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenRisksIsNull() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_selectedRisks_is_null.json",
-                "/rest/TravelCalculatePremiumResponse_selectedRisks_is_null.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenCountryDoesNotExist() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_country_does_not_exist.json",
-                "/rest/TravelCalculatePremiumResponse_country_does_not_exist.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenCountryIsNull() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_country_is_null.json",
-                "/rest/TravelCalculatePremiumResponse_country_is_null.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenCountryIsEmpty() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_country_not_provided.json",
-                "/rest/TravelCalculatePremiumResponse_country_not_provided.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenBirthDateIsNull() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_birthDate_is_null.json",
-                "/rest/TravelCalculatePremiumResponse_birthDate_is_null.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenBirthDateIsInTheFuture() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_birthDate_in_the_future.json",
-                "/rest/TravelCalculatePremiumResponse_birthDate_in_the_future.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenBirthDateExceedsLimit() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_birthDate_exceeds_limit.json",
-                "/rest/TravelCalculatePremiumResponse_birthDate_exceeds_limit.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenMedicalRiskLimitLevelNotExist() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_medicalRiskLimitLevel_does_not_exist.json",
-                "/rest/TravelCalculatePremiumResponse_medicalRiskLimitLevel_does_not_exist.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenMedicalRiskLimitLevelIsNull() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_medicalRiskLimitLevel_is_null.json",
-                "/rest/TravelCalculatePremiumResponse_medicalRiskLimitLevel_is_null.json");
-    }
-
-    @Test
-    void shouldReturnErrorWhenMedicalRiskLimitLevelIsEmpty() throws Exception {
-        performAndCheck("/rest/TravelCalculatePremiumRequest_medicalRiskLimitLevel_not_provided.json",
-                "/rest/TravelCalculatePremiumResponse_medicalRiskLimitLevel_not_provided.json");
     }
 }
