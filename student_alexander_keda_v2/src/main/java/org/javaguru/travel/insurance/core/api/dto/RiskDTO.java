3e36a29f80f344499127bf89153d3e67b6dc4385
Alexander Keda <alex.keda94@yandex.ru>
2025-04-14 11:28:30 +0300

    STEP 147 - DONE

--- student_alexander_keda_v2/src/main/java/org/javaguru/travel/insurance/core/api/dto/RiskDTO.java
+++ student_alexander_keda_v2/src/main/java/org/javaguru/travel/insurance/core/api/dto/RiskDTO.java
@@ -1,12 +1,14 @@
 package org.javaguru.travel.insurance.core.api.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class RiskDTO {
 
     private String riskIc;
---
+++ student_alexander_keda_v2/src/main/java/org/javaguru/travel/insurance/core/underwriting/SelectedRisksPremiumCalculator.java
@@ -0,0 +1,28 @@
+package org.javaguru.travel.insurance.core.underwriting;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import org.javaguru.travel.insurance.core.api.dto.PersonDTO;
+import org.javaguru.travel.insurance.core.api.dto.RiskDTO;
+import org.javaguru.travel.insurance.core.underwriting.calculators.TravelRiskPremiumCalculator;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+class SelectedRisksPremiumCalculator {
+
+    private final List<TravelRiskPremiumCalculator> travelRiskPremiumCalculators;
+
+    List<RiskDTO> calculatePremiumForAllRisks(AgreementDTO agreement, PersonDTO person) {
+        return travelRiskPremiumCalculators.stream()
+                .filter(riskCalculator -> agreement.getSelectedRisks().contains(riskCalculator.getRiskIc()))
+                .map(riskPremiumCalculator -> new RiskDTO(
+                        riskPremiumCalculator.getRiskIc(),
+                        riskPremiumCalculator.calculatePremium(agreement, person)))
+                .toList();
+    }
+
+}
