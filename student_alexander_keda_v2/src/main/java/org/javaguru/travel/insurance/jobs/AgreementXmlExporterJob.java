903c53a89e2c800f46a737a4e95407ca926b6659
Alexander Keda <alex.keda94@yandex.ru>
2025-07-10 13:04:52 +0300

    STEP 191 - DONE

--- student_alexander_keda_v2/src/main/java/org/javaguru/travel/insurance/jobs/AgreementXmlExporterJob.java
+++ student_alexander_keda_v2/src/main/java/org/javaguru/travel/insurance/jobs/AgreementXmlExporterJob.java
@@ -1,12 +1,18 @@
 package org.javaguru.travel.insurance.jobs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class JobExample {
+@ConditionalOnProperty(
+        name = "agreement.xml.exporter.job.enabled",
+        havingValue = "true",
+        matchIfMissing = false
+)
+public class AgreementXmlExporterJob {
 
     @Scheduled(fixedRate = 5000)
     public void doJob() {
